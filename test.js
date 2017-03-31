13efafa82b75c5840e8c693f4c933710cd18d5c0
yangran <sfyr111@163.com>
2017-04-01 00:26:10 +0800

    assert

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-const p = Promise.resolve(1)
+const assert = require('assert');
 
-const p1 = p.then(val => {
-  
+const p = Promise.resolve(1); 
+
+const p1 = p.then(val=>{
+	console.log(val); //1
+	return val + 1
+});// resolved 2
+
+const p2 = p1.then(val=>{
+	console.log(val);
+	assert.equal(val, 2) // 不相等报错
 })
